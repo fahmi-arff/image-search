3f5289f677a1dedd6878dbb41af618edfda35d89
fahmi-arff <fahmi.arff@gmail.com>
2019-09-04 11:09:10 +0700

    add event handler on typing

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  // to pick what type of input
+  onInputChange(event){
+    console.log(event.target.value);
+  }
+
   render(){
     return(
       <div className="ui segment">
         <form className="ui form"> 
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" onChange={this.onInputChange} />
+            {/* we do not set parathesis() on onInputChange
+            by leaving the () we are passing a reference to 
+            this function to input element to that the input
+            can call that function at some point in time in
+            future */}
           </div>
         </form>
       </div>
