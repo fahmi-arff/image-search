73139d2585c24c02e284b0e6195692c638e1b662
fahmi-arff <fahmi.arff@gmail.com>
2019-09-04 11:35:08 +0700

    refactor to controlled element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  // to pick what type of input
-  onInputChange(event){
-    console.log(event.target.value);
-  }
+  state = { term: '' };
 
   render(){
     return(
@@ -12,12 +9,11 @@ class SearchBar extends React.Component {
         <form className="ui form"> 
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
-            {/* we do not set parathesis() on onInputChange
-            by leaving the () we are passing a reference to 
-            this function to input element to that the input
-            can call that function at some point in time in
-            future */}
+            <input 
+              type="text" 
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })} 
+            />
           </div>
         </form>
       </div>
