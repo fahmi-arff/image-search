5545caa52f0d893961f8f724cba0e8d30a9abe7d
fahmi-arff <fahmi.arff@gmail.com>
2019-11-06 06:33:28 +0700

    prepare deploying

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { clientId } from '../config';
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID c59e14f9eb75a1ccba65ac8c6c564af19299c8cd474cae34566789061532f5a5'
+    Authorization: `Client-ID ${clientId}`
   }
 }); 
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const clientId = process.env.REACT_APP_CLIENT_ID;
